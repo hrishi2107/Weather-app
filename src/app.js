fbec1dba9a5eca3fb0169801f4aa780b162c0974
HRISHI PANDEY <hrishipandey193@gmail.com>
2022-12-27 00:27:46 +0530

    Setup app for railway hosting

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const path=require('path')
 const express = require('express')
 const hbs=require('hbs')
 const app=express()
+
+
 const forecast=require('./utils/forecast')
 const geocode=require('./utils/geocode')
 
+const port= process.env.PORT || 3000
 const publicDirectoryPath= path.join(__dirname,'../public')
 const viewsPath=path.join(__dirname,'../templates/views')
 const partialsPath=path.join(__dirname,'../templates/partials')
@@ -78,8 +81,8 @@ app.get('*',(req,res)=>{
 })
 
 
-app.listen(3000,()=>{
-    console.log('Server is up on port 3000')
+app.listen(port,()=>{
+    console.log('Server is up on port '+port)
 })
 
 
